42ed8eb5ae2c8843408f726c9a9f8ead24babdea
Kirill <samarinkirill63@gmail.com>
2026-07-22 03:41:38 +0300

    Refactor logger API; update CMake and build fixes
    Rename Logger API to lower-case (Info/Warn/Error/Critical/Module*) -> (info/warn/error/critical/module*) and LogMessage -> logMessage; remove unused static logger and sink include in Logger.cpp and call spdlog directly. Add remove_nostdlib_flags() helper in cmake/tools.cmake and apply it to the GL module; link triple::math to triple_gfx and add glfw to the GL target. Make FetchContent extraction deterministic (DOWNLOAD_EXTRACT_TIMESTAMP true) and enable IndentPPDirectives in .clang-format. These changes standardize naming, fix build/link flags, and improve reproducible fetches.

--- engine/log/src/Logger.cpp
+++ engine/log/src/Logger.cpp
@@ -1,27 +1,21 @@
 #include "triple/log/Logger.h"
 #include <spdlog/spdlog.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <memory>
-
-namespace {
-	static std::shared_ptr<spdlog::logger> s_logger;
-}
 
 namespace triple::log {
-	void Logger::LogMessage(LogLevel level, std::string_view message) {
+	void Logger::logMessage(LogLevel level, std::string_view message) {
 		switch (level) {
-		case LogLevel::Info:
-			spdlog::info(message);
-			break;
-		case LogLevel::Warn:
-			spdlog::warn(message);
-			break;
-		case LogLevel::Error:
-			spdlog::error(message);
-			break;
-		case LogLevel::Critical:
-			spdlog::critical(message);
-			break;
+			case LogLevel::Info:
+				spdlog::info(message);
+				break;
+			case LogLevel::Warn:
+				spdlog::warn(message);
+				break;
+			case LogLevel::Error:
+				spdlog::error(message);
+				break;
+			case LogLevel::Critical:
+				spdlog::critical(message);
+				break;
 		}
 	}
 } // namespace triple::log
